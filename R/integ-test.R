15354bae57b0be0c7792b4c9215a7050803ec3a5
Edgar Ruiz <edgararuiz@gmail.com>
2023-07-19 15:15:50 -0500

    Improves skip test routine

--- R/integ-test.R
+++ R/integ-test.R
@@ -1,25 +1,24 @@
 #' @export
 spark_integ_test_skip.pyspark_connection <- function(sc, test_name) {
+
+  supports <- function(x, test, default_out = FALSE) {
+    if (grepl(test, test_name)) {
+      x <- default_out
+    }
+    x
+  }
+
   out <- TRUE
-  if (grepl("dplyr", test_name)) out <- FALSE
-  if (grepl("dplyr-do", test_name)) out <- TRUE
-  if (grepl("dplyr-hof", test_name)) out <- TRUE
-  if (grepl("dplyr-cumprod", test_name)) out <- TRUE
 
-  if (grepl("sample-n-weights", test_name)) out <- TRUE
-  if (grepl("sample-n-replace", test_name)) out <- TRUE
-
-  if (grepl("sample-with-seed", test_name)) out <- TRUE
-
-  if (grepl("sample-frac-weights", test_name)) out <- TRUE
-  if (grepl("sample-frac-replace", test_name)) out <- TRUE
-  if (grepl("sample-frac-exact", test_name)) out <- TRUE
-
-  if (grepl("dbi", test_name)) out <- FALSE
-  if (grepl("sdf-broadcast", test_name)) out <- TRUE
-  if (grepl("ml-", test_name)) out <- TRUE
-
-  if (grepl("supports-nas", test_name)) out <- TRUE
-
-  out
+  out %>%
+    supports("dplyr") %>%
+    supports("dplyr-do", TRUE) %>%
+    supports("dplyr_hof", TRUE) %>%
+    supports("dplyr-cumprod", TRUE) %>%
+    supports("DBI") %>%
+    supports("format-csv") %>%
+    supports("format-parquet") %>%
+    supports("format-orc") %>%
+    supports("format-json") %>%
+    supports("format-text")
 }

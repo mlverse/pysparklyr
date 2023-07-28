2b8ac891b96e694b87860bf3cb3b0a9bc502532c
Edgar Ruiz <edgararuiz@gmail.com>
2023-07-28 14:25:01 -0500

    Split skip function into DB and Spark methods

--- R/integ-test.R
+++ R/integ-test.R
@@ -1,29 +1,38 @@
 #' @export
-spark_integ_test_skip.pyspark_connection <- function(sc, test_name) {
+spark_integ_test_skip.connect_databricks <- function(sc, test_name) {
+  spark_integ_generic(test_name)
+}
 
-  supports <- function(x, test, default_out = FALSE) {
-    if (grepl(test, test_name)) {
-      x <- default_out
-    }
-    x
-  }
+#' @export
+spark_integ_test_skip.connect_spark <- function(sc, test_name) {
+  spark_integ_generic(test_name) %>%
+    supports("format-csv", test_name) %>%
+    supports("format-parquet", test_name) %>%
+    supports("format-orc", test_name) %>%
+    supports("format-json", test_name) %>%
+    supports("format-text", test_name)
+}
 
+
+spark_integ_generic <- function(test_name) {
   out <- TRUE
 
   out %>%
-    supports("dplyr") %>%
-    supports("dplyr-do", TRUE) %>%
-    supports("dplyr-hof", TRUE) %>%
-    supports("dplyr-cumprod", TRUE) %>%
-    supports("DBI") %>%
-    supports("format-csv") %>%
-    supports("format-parquet") %>%
-    supports("format-orc") %>%
-    supports("format-json") %>%
-    supports("format-text") %>%
-    supports("pivot-longer") %>%
-    supports("pivot-longer-names-repair", TRUE) %>%
-    supports("pivot-longer-values-transform", TRUE) %>%
-    supports("ml-", TRUE) %>%
-    supports("sdf-distinct")
+    supports("dplyr", test_name) %>%
+    supports("dplyr-do", test_name, TRUE) %>%
+    supports("dplyr-hof", test_name, TRUE) %>%
+    supports("dplyr-cumprod", test_name, TRUE) %>%
+    supports("DBI", test_name) %>%
+    supports("pivot-longer", test_name) %>%
+    supports("pivot-longer-names-repair", test_name, TRUE) %>%
+    supports("pivot-longer-values-transform", test_name, TRUE) %>%
+    supports("ml-", test_name, TRUE) %>%
+    supports("sdf-distinct", test_name)
+}
+
+supports <- function(x, test, test_name, default_out = FALSE) {
+  if (grepl(test, test_name)) {
+    x <- default_out
+  }
+  x
 }

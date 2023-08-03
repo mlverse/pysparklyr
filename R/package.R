ce9495f2eb09baa8b1c959632471001c2e5234e1
Edgar Ruiz <edgararuiz@gmail.com>
2023-08-03 11:26:53 -0500

    Adds message to confirm it's trying to use that path

--- R/package.R
+++ R/package.R
@@ -34,6 +34,7 @@ NULL
   # RStudio inside the Databricks cluster environment
   db_path <- "/databricks/python/bin/python"
   if(file_exists(db_path)) {
+    cli_text(glue("Using Python: {db_path}"))
     use_python(db_path)
   }
   use_virtualenv("r-sparklyr", required = FALSE)

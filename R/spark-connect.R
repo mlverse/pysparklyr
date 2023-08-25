0943c1856a549c8769b0fa15a11656957f5f0177
Edgar Ruiz <edgararuiz@gmail.com>
2023-08-25 10:31:47 -0500

    Adds cachethreshold conf if Spark connect. Fixes #22

--- R/spark-connect.R
+++ R/spark-connect.R
@@ -88,6 +88,10 @@ py_spark_connect <- function(master,
 
   python <- conn$getOrCreate()
 
+  if(con_class == "connect_spark") {
+    python$conf$set("spark.sql.session.localRelationCacheThreshold", 1048576L)
+  }
+
   get_version <- try(python$version, silent = TRUE)
 
   if (inherits(get_version, "try-error")) {

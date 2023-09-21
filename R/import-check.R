a433659eae4a7a7b38f7b16ad37013e4e2b91356
Edgar Ruiz <edgararuiz@gmail.com>
2023-09-21 15:46:09 -0400

    Adds support for Conda in import_check()

--- R/import-check.R
+++ R/import-check.R
@@ -1,31 +1,37 @@
-import_check <- function(x, virtualenv_name) {
-  env_found <- virtualenv_name %in% virtualenv_list()
-
+import_check <- function(x, envname) {
+  env_found <- !is.na(envname)
   env_loaded <- NA
 
   if (py_available()) {
+    # If there is a Python environment already loaded
     if (env_found) {
-      if (virtualenv_python(virtualenv_name) == py_exe()) {
+      if (env_python(envname) == py_exe()) {
         env_loaded <- TRUE
       } else {
         env_loaded <- FALSE
       }
     }
   } else {
+    # If there is NO Python environment already loaded
     if (env_found) {
-      try(use_virtualenv(virtualenv_name), silent = TRUE)
+      # If the envname is found, we try to use it
+      if(env_type(envname) == "virtualenv") {
+        try(use_virtualenv(envname), silent = TRUE)
+      } else {
+        try(use_condaenv(envname), silent = TRUE)
+      }
     }
   }
 
   out <- try(import(x), silent = TRUE)
 
   if (is.na(env_loaded)) {
-    env_loaded <- virtualenv_python(virtualenv_name) == py_exe()
+    env_loaded <- env_python(envname) == py_exe()
   }
 
   inst <- paste0(
     " {.run pysparklyr::install_pyspark(",
-    "virtualenv_name = \"{virtualenv_name}\")}"
+    "envname = \"{envname}\")}"
   )
 
   if (inherits(out, "try-error")) {
@@ -33,13 +39,13 @@ import_check <- function(x, virtualenv_name) {
       if (env_loaded) {
         # found & loaded
         cli_abort(paste(
-          "Python library '{x}' is not available in the '{virtualenv_name}'",
+          "Python library '{x}' is not available in the '{envname}'",
           "virtual environment. Install all of the needed python libraries",
           "using:", inst
         ))
       } else {
         cli_abort(paste(
-          "Python library '{x}' is not available. The '{virtualenv_name}'",
+          "Python library '{x}' is not available. The '{envname}'",
           "virtual environment is installed, but it is not loaded.",
           "Restart your R session, and avoid initializing Python",
           "before using `pysparklyr`"
@@ -47,7 +53,7 @@ import_check <- function(x, virtualenv_name) {
       }
     } else {
       cli_abort(paste(
-        "Python library '{x}' not available. The '{virtualenv_name}'",
+        "Python library '{x}' not available. The '{envname}'",
         "virtual environment is not installed. Restart your R session,",
         "and run:", inst
       ))
@@ -56,7 +62,7 @@ import_check <- function(x, virtualenv_name) {
     if (is.null(pysparklyr_env$vars$python_init)) {
       if (env_loaded) {
         msg <- paste(
-          "Using the {.emph '{virtualenv_name}'} virtual",
+          "Using the {.emph '{envname}'} virtual",
           "environment {.class ({py_exe()})}"
         )
         cli_div(theme = cli_colors())
@@ -64,7 +70,7 @@ import_check <- function(x, virtualenv_name) {
         cli_end()
       } else {
         msg <- paste(
-          "Not using the '{virtualenv_name}' virtual environment",
+          "Not using the '{envname}' virtual environment",
           "for python. The current path is: {py_exe()}"
         )
         cli_alert_danger(msg)
@@ -74,4 +80,29 @@ import_check <- function(x, virtualenv_name) {
   }
 
   out
+}
+
+env_type <- function(envname) {
+  ret <- NA
+  if(envname %in% virtualenv_list()) {
+    ret <- "virtualenv"
+  }
+  if(envname %in% conda_list()$name && is.na(ret)) {
+    ret <- "conda"
+  }
+  ret
+}
+
+env_python <- function(envname) {
+  ret <- NA
+  type <- env_type(envname)
+  if(!is.na(type)) {
+    if(type == "virtualenv") {
+      ret <- virtualenv_python()
+    }
+    if(type == "conda") {
+      ret <- conda_python()
+    }
+  }
+  ret
 }

cc8264e8233a582ca6f43fe06b1486744f2267da
Edgar Ruiz <edgararuiz@gmail.com>
2023-09-30 11:29:35 -0500

    Adds testthat, and initial test

---
+++ tests/testthat/helper-init.R
@@ -0,0 +1,25 @@
+.test_env <- new.env()
+.test_env$sc <- NULL
+
+test_version_spark <- function() {
+  version <- Sys.getenv("SPARK_VERSION", unset = NA)
+  if(is.na(version)) version <- "3.4"
+  version
+}
+
+test_scala_spark <- function() {
+  version <- Sys.getenv("SCALA_VERSION", unset = NA)
+  if(is.na(version)) version <- "2.12"
+  version
+}
+
+test_spark_connect <- function() {
+  if(is.null(.test_env$sc)) {
+    .test_env$sc <- spark_connect(
+      master = "sc://localhost",
+      method = "spark_connect",
+      version = test_version_spark()
+    )
+  }
+  .test_env$sc
+}
---
+++ tests/testthat/test-dplyr.R
@@ -0,0 +1,10 @@
+test_that("copy_to() works", {
+  sc <- test_spark_connect()
+  expect_silent(
+    tbl_mtcars <- copy_to(sc, mtcars)
+  )
+  expect_snapshot(print(tbl_mtcars))
+
+  expect_snapshot(print(head(tbl_mtcars)))
+
+})

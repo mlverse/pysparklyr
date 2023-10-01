d6411c34652d3cd03e6f295f2ceb844254be78f1
Edgar Ruiz <edgararuiz@gmail.com>
2023-09-30 19:37:29 -0500

    Adds integ tests, and additional dplyr

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -7,4 +7,32 @@ test_that("copy_to() works", {
 
   expect_snapshot(print(head(tbl_mtcars)))
 
+  tbl_n <- tbl_mtcars %>%
+    sample_n(5) %>%
+    collect() %>%
+    count() %>%
+    pull()
+
+  expect_equal(tbl_n, 5)
+
+  tbl_frac <- tbl_mtcars %>%
+    sample_frac(0.2) %>%
+    collect() %>%
+    count() %>%
+    pull()
+
+  expect_lt(tbl_frac, 10)
+
+  tbl_am <- tbl_mtcars %>%
+    group_by(am) %>%
+    filter(mpg == max(mpg, na.rm = TRUE)) %>%
+    select(am)
+
+  expect_silent(compute(tbl_am, name = "am"))
+
+  tbl_join <- tbl_mtcars %>%
+    left_join(tbl_am, by = "am") %>%
+    arrange(mpg)
+
+  expect_snapshot(tbl_join)
 })

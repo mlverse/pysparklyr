37cf14e6eead186e959bb3530a53a1698bd79999
Edgar Ruiz <edgararuiz@gmail.com>
2023-10-01 14:36:08 -0500

    Updates version of actions/cache (hopefully fixing the caching venv issue), partitions dplyr tests to identify the failing ones

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -1,18 +1,15 @@
 test_that("copy_to() works", {
-  sc <- test_spark_connect()
-
-  expect_silent(
-    tbl_mtcars <- test_table_mtcars()
-  )
-
-  tbl_ordered <- tbl_mtcars %>%
+  tbl_ordered <- test_table_mtcars() %>%
     arrange(mpg)
 
   expect_snapshot(tbl_ordered)
 
   expect_snapshot(print(head(tbl_ordered)))
+})
 
-  tbl_n <- tbl_mtcars %>%
+test_that("Sampling functions works", {
+
+  tbl_n <- test_table_mtcars() %>%
     sample_n(5) %>%
     collect() %>%
     count() %>%
@@ -20,22 +17,25 @@ test_that("copy_to() works", {
 
   expect_equal(tbl_n, 5)
 
-  tbl_frac <- tbl_mtcars %>%
+  tbl_frac <- test_table_mtcars() %>%
     sample_frac(0.2) %>%
     collect() %>%
     count() %>%
     pull()
 
   expect_lt(tbl_frac, 30)
+})
 
-  tbl_am <- tbl_mtcars %>%
+test_that("Misc functions", {
+
+  tbl_am <- test_table_mtcars() %>%
     group_by(am) %>%
     filter(mpg == max(mpg, na.rm = TRUE)) %>%
     select(am)
 
   expect_silent(compute(tbl_am, name = "am"))
 
-  tbl_join <- tbl_mtcars %>%
+  tbl_join <- test_table_mtcars() %>%
     left_join(tbl_am, by = "am") %>%
     arrange(mpg)
 

d964704164fb5b9755dc73d80dec361775187a69
Edgar Ruiz <edgararuiz@gmail.com>
2023-10-02 13:22:53 -0500

    Adds more tests to pivot_longer()

--- tests/testthat/helper-init.R
+++ tests/testthat/helper-init.R
@@ -38,3 +38,41 @@ test_table_mtcars <- function() {
 test_coverage_enable <-  function() {
   Sys.setenv("CODE_COVERAGE" = "true")
 }
+
+expect_same_remote_result <- function(.data, pipeline) {
+  sc <- test_spark_connect()
+  temp_name <- random_table_name("test_")
+  spark_data <- copy_to(sc, .data, temp_name)
+
+  local <- pipeline(.data)
+
+  remote <- try(
+    spark_data %>%
+      pipeline() %>%
+      collect()
+  )
+
+  if(inherits(remote, "try-error")) {
+    expect_equal(remote[[1]], "")
+  } else {
+    expect_equal(local, remote, ignore_attr = TRUE)
+  }
+
+  DBI::dbRemoveTable(sc, temp_name)
+}
+
+testthat_tbl <- function(name, data = NULL, repartition = 0L) {
+  sc <- test_spark_connect()
+
+  tbl <- tryCatch(dplyr::tbl(sc, name), error = identity)
+  if (inherits(tbl, "error")) {
+    if (is.null(data)) data <- eval(as.name(name), envir = parent.frame())
+    tbl <- dplyr::copy_to(sc, data, name = name, repartition = repartition)
+  }
+
+  tbl
+}
+
+random_table_name <- function(prefix) {
+  paste0(prefix, paste0(floor(runif(10, 0, 10)), collapse = ""))
+}

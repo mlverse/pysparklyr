5730d122213cca8f2aae0fbfda35a3fe9b28846c
Edgar Ruiz <edgararuiz@gmail.com>
2023-10-16 15:01:33 -0500

    Improves print out

--- R/ml-pipeline.R
+++ R/ml-pipeline.R
@@ -7,7 +7,7 @@ ml_pipeline.pyspark_connection <- function(x, ..., uid = NULL) {
 
 ml_torch_add_stage <- function(x, stage) {
   pipeline <- x$.jobj$pyspark_obj$Pipeline
-  stage_print <- r_to_py("testing") # ml_print_params(stage)
+  stage_print <- ml_print_params(stage)
   if(inherits(pipeline, "pyspark.ml.connect.pipeline.Pipeline")) {
     stages <- pipeline$getStages()
     outputs <- c(x$stages, list(stage_print))
@@ -40,7 +40,17 @@ as_pipeline <- function(jobj, outputs = NULL, get_uid = FALSE) {
   )
 }
 
+ml_get_last_item <- function(x) {
+  classes <- x %>%
+    strsplit("\\.") %>%
+    unlist()
+
+  classes[length(classes)]
+}
+
 ml_print_params <-  function(x) {
+  class_1 <- ml_get_last_item(class(x)[[1]])
+  class_2 <- ml_get_last_item(class(x)[[3]])
   x_params <- x$params %>%
     map_chr(~ {
       nm <- .x$name
@@ -55,5 +65,19 @@ ml_print_params <-  function(x) {
       tr
     })
   x_params <- x_params[x_params != ""]
-  paste0(x_params, collapse = "\n")
+  name_label <- paste0("<", capture.output(x), ">")
+  ret <- paste0(x_params, collapse = "\n")
+  ret <- paste0(
+    class_1, " (", class_2, ")", "\n",
+    name_label, "\n",
+    "(Parameters)\n",
+    ret
+    )
+  class(ret) <- "ml_output_params"
+  ret
+}
+
+#' @export
+print.ml_output_params <- function(x,...) {
+  cat(x)
 }

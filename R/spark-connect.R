b5d020ee95be36913cc4c26bf9a74426df2d9b2f
Edgar Ruiz <edgararuiz@gmail.com>
2023-11-01 16:54:56 -0500

    Fixes and improvements to messages

--- R/spark-connect.R
+++ R/spark-connect.R
@@ -322,10 +322,10 @@ sanitize_host <-  function(url) {
   if(ret != url) {
     cli_div(theme = cli_colors())
     cli_alert_warning(
-      c("Sanitizing {.code Host} value:\n",
-        "|- Original: {.emph {url}}\n",
-        "|- Using:    {.emph {ret}}\n",
-        "|- {.header ",
+      c("{.header Sanitizing {.code Host} value:}\n",
+        "{.header * Original:} {.emph {url}}\n",
+        "{.header * Using:}    {.emph {ret}}\n",
+        "* {.header ",
         "To prevent {.code sparklyr} from changing the Host, set ",
         "{.code host_sanitize = FALSE} in {.code spark_connect()}",
         "}"
@@ -346,7 +346,7 @@ use_envname <- function(
   reticulate_python <- Sys.getenv("RETICULATE_PYTHON", unset = NA)
   if (!is.na(reticulate_python)) {
     if(messages) {
-      msg <- paste(
+      msg <- c(
         "{.header Using the Python environment defined in the}",
         "{.emph 'RETICULATE_PYTHON' }{.header environment variable}",
         "{.class ({py_exe()})}"
@@ -361,26 +361,24 @@ use_envname <- function(
   if (is.null(envname)) {
     if(method == "spark_connect") {
       env_base <- "r-sparklyr-pyspark-"
-      run_code <- paste(
-        "{.run pysparklyr::install_pyspark(version = \"{version}\")} to install."
+      run_code <- glue(
+        "pysparklyr::install_pyspark(version = \"{version}\")"
         )
     } else {
       env_base <- "r-sparklyr-databricks-"
-      run_code <- paste(
-        "{.run pysparklyr::install_databricks(version = \"{version}\")} to install."
+      run_code <- glue(
+        "pysparklyr::install_databricks(version = \"{version}\")"
       )
     }
     envs <- find_environments(env_base)
     if (length(envs) == 0) {
       if(messages) {
         cli_div(theme = cli_colors())
-        cli_abort(c(
-          paste0(
-            "{.header No environment name provided, and }",
-            "{.header  no environment was automatically identified.}"
-          ),
-          paste0("Run: {run_code}")
-        ))
+        cli_abort(
+          c(
+            "{.header No environment name provided, and no environment was automatically identified.}",
+            "* {.header Run: {.run {run_code}} to install.}"
+            ))
         cli_end()
       }
     } else {
@@ -395,10 +393,7 @@ use_envname <- function(
             cli_alert_warning(paste(
               "{.header A Python environment with a matching version was not found}",
               "* {.header Will attempt connecting using }{.emph '{envname}'}",
-              paste0(
-                "* {.header To install the proper Python environment use:}",
-                run_code
-              ),
+              "* {.header To install the proper Python environment use: {.run {run_code}}}",
               sep = "\n"
             ))
             cli_end()

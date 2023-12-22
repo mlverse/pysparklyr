8a68b17a1551267a31b1d130386d9f9e755236d9
Edgar Ruiz <edgararuiz@gmail.com>
2023-12-22 14:10:32 -0600

    Uses test python env

--- tests/testthat/test-zzz-spark-connect.R
+++ tests/testthat/test-zzz-spark-connect.R
@@ -16,20 +16,23 @@ test_that("build_user_agent() outputs work", {
 skip_if_not_databricks()
 
 test_that("Test Databricks connection", {
-  local_mocked_bindings(
-    initialize_connection = function(...) list(...)
-    )
-  py_install("databricks.connect")
-  sc <- spark_connect_method.spark_method_databricks_connect(
-    master = NULL,
-    method = "databricks_connect",
-    config = NULL
-    )
-  expect_s3_class(sc$conn, "databricks.connect.session.Builder")
-  expect_equal(sc$con_class, "connect_databricks")
-  expect_equal(sc$cluster_id, Sys.getenv("DATABRICKS_CLUSTER_ID"))
-  expect_equal(sc$method, "databricks_connect")
-  expect_null(sc$config)
+  withr::with_envvar(
+    new = c("WORKON_HOME" = use_test_env()),
+    {
+      local_mocked_bindings(
+        initialize_connection = function(...) list(...)
+      )
+      py_install("databricks.connect")
+      sc <- spark_connect_method.spark_method_databricks_connect(
+        master = NULL,
+        method = "databricks_connect",
+        config = NULL
+      )
+      expect_s3_class(sc$conn, "databricks.connect.session.Builder")
+      expect_equal(sc$con_class, "connect_databricks")
+      expect_equal(sc$cluster_id, Sys.getenv("DATABRICKS_CLUSTER_ID"))
+      expect_equal(sc$method, "databricks_connect")
+      expect_null(sc$config)
+    }
+  )
 })
-
-

a4aee2262c05842034639886485ee7eb0a58402a
Edgar Ruiz <edgararuiz@gmail.com>
2023-12-22 13:20:13 -0600

    Adds databricks connection test, with mocking

--- tests/testthat/test-spark-connect.R
+++ tests/testthat/test-spark-connect.R
@@ -12,3 +12,23 @@ test_that("build_user_agent() outputs work", {
   expect_equal(build_user_agent(), "testagent")
   Sys.unsetenv("SPARK_CONNECT_USER_AGENT")
 })
+
+skip_if_not_databricks()
+
+test_that("Test Databricks connection", {
+  local_mocked_bindings(
+    initialize_connection = function(...) list(...)
+    )
+  sc <- spark_connect_method.spark_method_databricks_connect(
+    master = NULL,
+    method = "databricks_connect",
+    config = NULL
+    )
+  expect_s3_class(sc$conn, "databricks.connect.session.Builder")
+  expect_equal(sc$con_class, "connect_databricks")
+  expect_equal(sc$cluster_id, Sys.getenv("DATABRICKS_CLUSTER_ID"))
+  expect_equal(sc$method, "databricks_connect")
+  expect_null(sc$config)
+})
+
+

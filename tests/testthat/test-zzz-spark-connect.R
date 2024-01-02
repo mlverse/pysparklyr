67f6e2ece1528bc8f8ec54916025d9f334760a71
Edgar Ruiz <edgararuiz@gmail.com>
2024-01-02 14:53:54 -0600

    Moves User Agent builder tests to own script, adds more tests for coverage

--- tests/testthat/test-zzz-spark-connect.R
+++ tests/testthat/test-zzz-spark-connect.R
@@ -1,18 +1,3 @@
-test_that("build_user_agent() outputs work", {
-  x <- build_user_agent()
-  expect_equal(substr(x, 1, 8), "sparklyr")
-
-  Sys.setenv("RSTUDIO_PRODUCT" = "CONNECT")
-  expect_equal(
-    build_user_agent(),
-    paste0("sparklyr/", packageVersion("sparklyr"), " posit-connect")
-  )
-
-  Sys.setenv("SPARK_CONNECT_USER_AGENT" = "testagent")
-  expect_equal(build_user_agent(), "testagent")
-  Sys.unsetenv("SPARK_CONNECT_USER_AGENT")
-})
-
 skip_if_not_databricks()
 
 test_that("Test Databricks connection", {

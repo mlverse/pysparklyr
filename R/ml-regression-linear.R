ae13a5192176121ebc39e57a16d2974df343fece
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-08-20 18:14:57 -0500

    Adds the other methods to linear regression

--- R/ml-regression-linear.R
+++ R/ml-regression-linear.R
@@ -39,6 +39,36 @@ ml_linear_regression.tbl_pyspark <- function(x, formula = NULL, fit_intercept = 
   )
 }
 
+#' @export
+ml_linear_regression.ml_connect_pipeline <- function(x, formula = NULL, fit_intercept = TRUE,
+                                                     elastic_net_param = 0, reg_param = 0,
+                                                     max_iter = 100, weight_col = NULL,
+                                                     loss = "squaredError", solver = "auto",
+                                                     standardization = TRUE, tol = 1e-6,
+                                                     features_col = "features", label_col = "label",
+                                                     prediction_col = "prediction",
+                                                     uid = NULL, ...) {
+  args <- c(as.list(environment()), list(...))
+  model <- ml_linear_regression_prep(args)
+  ml_connect_add_stage(
+    x = x,
+    stage = python_obj_get(model)
+  )
+}
+
+#' @export
+ml_linear_regression.pyspark_connection <- function(x, formula = NULL, fit_intercept = TRUE,
+                                                    elastic_net_param = 0, reg_param = 0,
+                                                    max_iter = 100, weight_col = NULL,
+                                                    loss = "squaredError", solver = "auto",
+                                                    standardization = TRUE, tol = 1e-6,
+                                                    features_col = "features", label_col = "label",
+                                                    prediction_col = "prediction",
+                                                    uid = NULL, ...) {
+  args <- c(as.list(environment()), list(...))
+  ml_linear_regression_prep(args)
+}
+
 ml_linear_regression_prep <- function(args) {
   jobj <- ml_execute(
     args = args,

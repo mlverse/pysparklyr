3f0a256c53b61e95b446d61d3daf20ffc8a934bf
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-09-02 14:54:26 -0500

    Adds multiclass evaluator

--- R/ml-evaluators.R
+++ R/ml-evaluators.R
@@ -12,3 +12,18 @@ ml_binary_classification_evaluator_impl <- function(
 ml_binary_classification_evaluator.pyspark_connection <- ml_binary_classification_evaluator_impl
 #' @export
 ml_binary_classification_evaluator.tbl_pyspark <- ml_binary_classification_evaluator_impl
+
+ml_multiclass_classification_evaluator_impl <- function(
+    x, label_col = "label", prediction_col = "prediction",
+    metric_name = "f1", uid = NULL, ...) {
+  ml_process_fn(
+    args = c(as.list(environment()), list(...)),
+    fn = "MulticlassClassificationEvaluator",
+    has_fit = FALSE,
+    ml_type = "evaluation"
+  )
+}
+#' @export
+ml_multiclass_classification_evaluator.pyspark_connection <- ml_multiclass_classification_evaluator_impl
+#' @export
+ml_multiclass_classification_evaluator.tbl_pyspark <- ml_multiclass_classification_evaluator_impl

557078922f9b285a61d62201e6cd2c37b03f9811
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-09-03 16:23:46 -0500

    Simplifies main processing ml function

--- R/ml-process.R
+++ R/ml-process.R
@@ -33,27 +33,38 @@ ml_process_fn <- function(args, fn, has_fit = TRUE, ml_type = "", ml_fn = NULL) 
     return(ml_connect_add_stage(x, stage))
   }
   if (inherits(x, "tbl_pyspark")) {
-    arg_formula <- NULL
-    if (ml_type != "feature") {
-      arg_formula <- args[["formula"]]
-    }
-    arg_label_col <- args[["label_col"]]
-    arg_features_col <- args[["input_col"]] %||%
-      args[["features_col"]] %||%
-      args[["raw_prediction_col"]]
-    new_obj <- ml_process_tbl(
+    tbl_prep <- ml_prep_dataset(
       x = x,
-      stage = stage,
-      formula = arg_formula,
-      label_col = arg_label_col,
-      features_col = arg_features_col,
-      has_fit = has_fit,
-      ml_type = ml_type,
-      ml_fn = ml_fn
+      formula = args[["formula"]],
+      label_col = args[["label_col"]],
+      features_col = args[["features_col"]] %||% args[["raw_prediction_col"]],
+      lf = "only"
     )
-    return(new_obj)
+    if (has_fit) {
+      stage <- ml_fit_impl(stage, tbl_prep)
+    }
+    if (ml_type == "evaluation") {
+      # TODO: Output will probably need to be expanded to a tbl
+      ret <- invoke(stage, "evaluate", python_obj_get(x))
+    } else {
+      attrs <- attributes(tbl_prep)
+      ret <- structure(
+        list(
+          pipeline = stage,
+          features = attrs$features,
+          label = attrs$label
+        ),
+        class = c(
+          "ml_connect_model",
+          glue("ml_model_{ml_fn}"),
+          glue("ml_model_{ml_type}"),
+          "ml_model_prediction",
+          "ml_model"
+        )
+      )
+    }
+    return(ret)
   }
-  abort("Object not recognized")
 }
 
 ml_process_transformer <- function(args, fn, has_fit = TRUE) {
@@ -73,10 +84,10 @@ ml_process_transformer <- function(args, fn, has_fit = TRUE) {
     sc = conn
   )
   prep_obj <- list(
-      uid = invoke(jobj, "uid"),
-      param_map = ml_get_params(jobj),
-      .jobj = jobj
-    )
+    uid = invoke(jobj, "uid"),
+    param_map = ml_get_params(jobj),
+    .jobj = jobj
+  )
   obj_class <- "ml_pipeline_stage"
   stage <- structure(
     prep_obj,
@@ -110,53 +121,4 @@ ml_process_transformer <- function(args, fn, has_fit = TRUE) {
     )
   }
   return(ret)
-}
-
-
-ml_process_tbl <- function(x, stage, formula, label_col, features_col,
-                           has_fit, ml_type = "feature", ml_fn) {
-  tbl_prep <- ml_prep_dataset(
-    x = x,
-    formula = formula,
-    label_col = label_col,
-    features_col = features_col,
-    lf = ifelse(ml_type == "feature", "all", "only")
-  )
-  if (has_fit) {
-    stage <- ml_fit_impl(stage, tbl_prep)
-  }
-
-  if (ml_type == "feature") {
-    conn <- spark_connection(x)
-    ret <- transform_impl(
-      x = stage,
-      dataset = tbl_prep,
-      prep = FALSE,
-      remove = FALSE,
-      conn = conn,
-      as_df = FALSE
-    )
-    ret <- tbl_pyspark_temp(ret, conn)
-  } else if (ml_type == "evaluation") {
-    # TODO: Output will probably need to be expanded to a tbl
-    ret <- invoke(stage, "evaluate", python_obj_get(x))
-  } else {
-    attrs <- attributes(tbl_prep)
-    ret <- structure(
-      list(
-        pipeline = stage,
-        features = attrs$features,
-        label = attrs$label
-      ),
-      class = c(
-        "ml_connect_model",
-        glue("ml_model_{ml_fn}"),
-        glue("ml_model_{ml_type}"),
-        "ml_model_prediction",
-        "ml_model"
-      )
-    )
-  }
-
-  ret
 }

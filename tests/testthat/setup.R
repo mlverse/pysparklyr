665b6c8ce1d44f26350bf7ca225a17f6384394d0
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-10-27 14:45:17 -0500

    Sets coverage flag to true

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessages(library(cli))
   # Disconnecting from Spark
   #withr::defer(spark_disconnect_all(), teardown_env())
   # Stopping Spark Connect service
-  withr::defer(pysparklyr::spark_connect_service_stop("4.0.1"))
+  #withr::defer(pysparklyr::spark_connect_service_stop("4.0.1"))
   # Deleting main Python environment
   #withr::defer(fs::dir_delete(use_test_env()), teardown_env())
 #})

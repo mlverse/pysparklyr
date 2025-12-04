f0c91087b580b55591cca20a708e03e4c21379e8
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-12-04 14:42:45 -0600

    Addresses rpy2 deprecation warnings, and issue with context

--- inst/udf/udf-apply.py
+++ inst/udf/udf-apply.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+
 def r_apply(pdf: pd.DataFrame) -> pd.DataFrame:
   pandas2ri.activate()
   r_func =robjects.r('''function(...) 1''')
-  ret = r_func(pdf)
-  return pandas2ri.rpy2py_dataframe(ret)
+  with localconverter(robjects.default_converter + pandas2ri.converter):
+          ret = r_func(pdf)
+          return robjects.conversion.rpy2py(ret)

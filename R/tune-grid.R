2a3df1397bd5bfe454a01d6b7d93a667f7e2eb2e
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-12-11 10:43:11 -0600

    Switches function to spark_tune_grid()

--- R/tune-grid.R
+++ R/tune-grid.R
@@ -1,3 +1,94 @@
+spark_tune_grid <- function(
+    object,
+    preprocessor,
+    resamples,
+    ...,
+    param_info = NULL,
+    grid = 10,
+    metrics = NULL,
+    eval_time = NULL,
+    control = control_grid(),
+    sc
+) {
+  # Get the location in the Spark driver where the files will be
+  # temporarily uploaded to
+  root_folder <- spark_session_root_folder(sc)
+
+  # Hashing R objects one time and using it for both the upload and
+  # reading inside the UDF script. This is done to avoid uploading the
+  # same files over and over again during the Spark session
+  hash_preprocessor <- rlang::hash(preprocessor)
+  hash_model <- rlang::hash(object)
+  hash_resamples <- rlang::hash(resamples)
+
+  # Copying R objects to Spark session
+  spark_session_add_file(preprocessor, sc, hash_preprocessor)
+  spark_session_add_file(object, sc, hash_model)
+  spark_session_add_file(resamples, sc, hash_resamples)
+
+  # De-parsing the code and updating the file and folder references
+  grid_code <- paste0(deparse(loop_call), collapse = "\n")
+  grid_code <- sub("preprocessing.rds", path(hash_preprocessor, ext = "rds"), grid_code)
+  grid_code <- sub("model.rds", path(hash_model, ext = "rds"), grid_code)
+  grid_code <- sub("resamples.rds", path(hash_resamples, ext = "rds"), grid_code)
+  grid_code <- sub("path/to/root", root_folder, grid_code)
+
+  # Creating the tune grid data frame
+  full_grid <- data.frame()
+  for (i in seq_along(resamples$splits)) {
+    temp_grid <- grid
+    temp_grid$index <- i
+    temp_grid$id <- resamples$id[[i]]
+    full_grid <- rbind(full_grid, temp_grid)
+  }
+
+  # Copies the grid to the Spark session. This is needed so that
+  # spark_apply() can recognize and use the table to set up each
+  # iteration
+  tbl_grid <- copy_to(
+    sc,
+    df = full_grid,
+    name = "pysparklyrtempgrid",
+    overwrite = TRUE
+  )
+
+  cols <- paste0(
+    "num_comp integer, tree_depth integer, metric string,",
+    " estimator string, estimate double, id string"
+  )
+
+  # Runs the code against the copies grid
+  tuned_results <- tbl_grid |>
+    spark_apply(
+      f = grid_code,
+      columns = cols
+    ) |>
+    collect()
+
+  # Converts metrics to list separated by id's
+  res_names <- colnames(tuned_results)
+  metrics_names <- res_names[res_names != "id"]
+  metrics_map <- map(
+    unique(tuned_results$id),
+    \(x) tuned_results[tuned_results$id == x, metrics_names]
+  ) |>
+    set_names(unique(tuned_results$id))
+
+  # Creates dummy 'notes' table
+  notes <- list(tibble(
+    location = character(0),
+    type = character(0),
+    note = character(0),
+    trace = list()
+  ))
+
+  resamples |>
+    mutate(
+      .metrics = metrics_map,
+      .notes = notes
+    )
+}
+
 spark_session_root_folder <- function(sc) {
   # A unique GUID is assigned to the Spark connection
   # this is used to pull the root folder only one time. It will save it to
@@ -47,69 +138,6 @@ spark_session_add_file <- function(x, sc, file_name = NULL) {
     pysparklyr_env[["artifacts"]][[connection_id]]$files <- c(file_name, files)
   }
   invisible()
-}
-
-spark_tune_grid_impl <- function(
-  sc,
-  preprocessor = NULL,
-  model = NULL,
-  resamples = NULL,
-  grid = NULL
-) {
-  # Get the location in the Spark driver where the files will be
-  # temporarily uploaded to
-  root_folder <- spark_session_root_folder(sc)
-
-  # Hashing R objects one time and using it for both the upload and
-  # reading inside the UDF script. This is done to avoid uploading the
-  # same files over and over again during the Spark session
-  hash_preprocessor <- rlang::hash(preprocessor)
-  hash_model <- rlang::hash(model)
-  hash_resamples <- rlang::hash(resamples)
-
-  # Copying R objects to Spark session
-  spark_session_add_file(preprocessor, sc, hash_preprocessor)
-  spark_session_add_file(model, sc, hash_model)
-  spark_session_add_file(resamples, sc, hash_resamples)
-
-  # De-parsing the code and updating the file and folder references
-  grid_code <- paste0(deparse(loop_call), collapse = "\n")
-  grid_code <- sub("preprocessing.rds", path(hash_preprocessor, ext = "rds"), grid_code)
-  grid_code <- sub("model.rds", path(hash_model, ext = "rds"), grid_code)
-  grid_code <- sub("resamples.rds", path(hash_resamples, ext = "rds"), grid_code)
-  grid_code <- sub("path/to/root", root_folder, grid_code)
-
-  # Creating the tune grid data frame
-  full_grid <- data.frame()
-  for (i in seq_along(resamples$splits)) {
-    temp_grid <- grid
-    temp_grid$index <- i
-    temp_grid$id <- resamples$id[[i]]
-    full_grid <- rbind(full_grid, temp_grid)
-  }
-
-  # Copies the grid to the Spark session. This is needed so that
-  # spark_apply() can recognize and use the table to set up each
-  # iteration
-  temp_grid <- copy_to(
-    sc,
-    df = full_grid,
-    name = "pysparklyrtempgrid",
-    overwrite = TRUE
-  )
-
-  cols <- paste0(
-    "num_comp integer, tree_depth integer, metric string,",
-    " estimator string, estimate double, id string"
-  )
-
-  # Runs the code against the copies grid
-  temp_grid |>
-    spark_apply(
-      f = grid_code,
-      columns = cols
-    ) |>
-    collect()
 }
 
 # `x` only contains a table with the grid containing every single combination

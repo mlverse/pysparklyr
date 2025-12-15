a2420da6895a78a33a398d7ec7118cde77358a23
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-12-15 16:05:46 -0600

    First successful run inside Spark

--- R/tune-grid.R
+++ R/tune-grid.R
@@ -8,7 +8,7 @@ spark_tune_grid <- function(
   grid = 10,
   metrics = NULL,
   eval_time = NULL,
-  control = control_grid(parallel_over = "everything"),
+  control = control_grid(parallel_over = "resamples"),
   sc,
   grid_partitions = NULL
 ) {
@@ -39,7 +39,7 @@ spark_tune_grid <- function(
 
   # Uploads the files to the Spark temp folder, this function skips the upload
   # if the hashed file name has already been uploaded during the current session
-  spark_session_add_file(wf, sc, hash_static)
+  spark_session_add_file(static, sc, hash_static)
   spark_session_add_file(resamples, sc, hash_resamples)
   spark_session_add_file(.Random.seed, sc, hash_r_seed)
 
@@ -79,7 +79,7 @@ spark_tune_grid <- function(
     }
   ) |>
     paste0(collapse = " ") |>
-    paste("metric string, estimator string, estimate double, index integer")
+    paste("metric string, estimator string, estimate double, config string")
 
   sc_obj <- spark_session(sc)
 
@@ -87,7 +87,7 @@ spark_tune_grid <- function(
   # The grid is copied to Spark, it will be repartitioned if `grid_paritions`
   # is set. If not set, Spark will decide how many partitions the data will have,
   # that impacts how many discrete jobs there will be set for this run
-  tbl_grid <- sc_obj$createDataFrame(full_grid[1, colnames(full_grid) != "id"])
+  tbl_grid <- sc_obj$createDataFrame(full_grid[, colnames(full_grid) != "id"])
   if (!is.null(grid_partitions)) {
     tbl_grid <- tbl_grid$repartition(as.integer(grid_partitions))
   }
@@ -101,6 +101,8 @@ spark_tune_grid <- function(
       .as_sdf = FALSE,
     ) |>
     collect()
+
+  return(tuned_results)
 
   # Finalizes metrics tables by adding the 'id' label, and `.config`, and
   # restoring the 'dot' prefix to the metric fields (Spark does not like
@@ -169,63 +171,14 @@ loop_call <- function(x) {
   # will depend on how the grid data frame was partitioned inside Spark.
   for (i in seq_len(nrow(x))) {
     curr_x <- x[i, ]
-    curr_resample <- resamples[curr_x$index, ]
+    curr_resample <- resamples[1, ]
     curr_resample <- dplyr::mutate(curr_resample, .seeds = list(list()))
     curr_grid <- curr_x[, colnames(curr_x) != "index"]
+    curr_grid <- as_tibble(curr_grid)
+    #curr_grid <- tibble::tibble(num_comp = 1, tree_depth = 1)
     res <- tune:::loop_over_all_stages(curr_resample, curr_grid, static)
     res_df <- Reduce(rbind, res$.metrics)
     out <- rbind(out, res_df)
   }
   out
-}
-
-spark_session_root_folder <- function(sc) {
-  # A unique GUID is assigned to the Spark connection
-  # this is used to pull the root folder only one time. It will save it to
-  # a package level environment variable that will be retained while the
-  # connection is current.
-  connection_id <- sc[["connection_id"]]
-  artifacts <- pysparklyr_env[["artifacts"]][[connection_id]]
-  if (is.null(artifacts)) {
-    py_run_string("
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
-from pyspark import SparkFiles
-def get_root_dir(dummy_input):
-    root_dir = SparkFiles.getRootDirectory()
-    return f'{root_dir}'
-root_dir_udf = udf(get_root_dir, StringType())
-")
-    main <- reticulate::import_main()
-    sc_obj <- spark_session(sc)
-    temp_df <- sc_obj$createDataFrame(data.frame(a = 1))
-    root_df <- temp_df$withColumn("root_folder", main$root_dir_udf("a"))$collect()
-    root_folder <- root_df[[1]]$asDict()$root_folder
-    artifacts <- list(root_folder = root_folder, files = c())
-    pysparklyr_env$artifacts[[connection_id]] <- artifacts
-  }
-  pysparklyr_env$artifacts[[connection_id]]$root_folder
-}
-
-spark_session_add_file <- function(x, sc, file_name = NULL) {
-  # Tracks in a environment variable which file names have already been
-  # uploaded to avoid doing it again. This is why the hashed values of
-  # the object in `x` is preferred, which ensure proper tracking of the
-  # actual objects that have been uploaded
-  invisible(spark_session_root_folder(sc))
-  connection_id <- sc[["connection_id"]]
-  artifacts <- pysparklyr_env[["artifacts"]][[connection_id]]
-  if (is.null(file_name)) {
-    file_name <- rlang::hash(x)
-  }
-  files <- artifacts[["files"]]
-  if (!file_name %in% artifacts[["files"]]) {
-    temp_file <- path_temp(file_name, ext = "rds")
-    saveRDS(x, temp_file)
-    sc_obj <- spark_session(sc)
-    sc_obj$addArtifact(temp_file, file = TRUE)
-    file_delete(temp_file)
-    pysparklyr_env[["artifacts"]][[connection_id]]$files <- c(file_name, files)
-  }
-  invisible()
 }

e41f1a4042bb944241c7cb95699b51305473ca34
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2026-01-15 12:23:00 -0600

    Adds helper function that returns a tuned grid and its components

--- tests/testthat/helper-ml.R
+++ tests/testthat/helper-ml.R
@@ -34,3 +34,37 @@ use_test_mtcars_va <- function() {
       output_col = "vec_x"
     )
 }
+
+
+# ------------------------------ Tune Grid -------------------------------------
+
+use_tune_grid <- function() {
+  if(is.null(.test_env$tune_grid)) {
+    out <- list()
+    set.seed(1010110)
+    object <- parsnip::decision_tree(tree_depth = tune()) |>
+      parsnip::set_mode("classification") |>
+      parsnip::set_engine("rpart")
+    preprocessor <- recipes::recipe(Species ~ ., data = iris) |>
+      recipes::step_normalize(recipes::all_predictors()) |>
+      recipes::step_pca(recipes::all_numeric_predictors(), num_comp = tune::tune())
+    resamples <- rsample::bootstraps(iris, 5)
+    wf_grid <- dials::grid_regular(dials::num_comp(c(1L, 15L)), dials::tree_depth())
+    cntrl <- tune::control_grid(verbose = TRUE, save_pred = TRUE)
+    results <- tune::tune_grid(
+      object = object,
+      preprocessor = preprocessor,
+      resamples = resamples,
+      grid = wf_grid,
+      control = cntrl
+      )
+    out$object <- object
+    out$preprocessor <- preprocessor
+    out$resamples <- resamples
+    out$grid <- wf_grid
+    out$control <- cntrl
+    out$results <- results
+    .test_env$tune_grid <- out
+  }
+  .test_env$tune_grid
+}
